ffdc775e7e582933d6a6676c3e0f5f68fb08d836
ghaith990000 <ghaith990000@gmail.com>
2024-04-26 14:13:18 +0300

    adding delete functionality when tapping on list item

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText){
-    setEnteredGoalText(enteredText);
-  };
 
-  function addGoalHandler(){
+  function addGoalHandler(enteredGoalText){
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals, 
       {text: enteredGoalText, id: Math.random().toString()},
     ]);
   };
 
+  function deleteGoalHandler(id){
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
-      
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList 
           data={courseGoals} 
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text}/>
+            return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler}/>
           }}
           keyExtractor={(item, index) =>{
             return item.id
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { Button, StyleSheet, TextInput, View } from "react-native";
+ 
+function GoalInput(props) {
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+    function goalInputHandler(enteredText){
+        setEnteredGoalText(enteredText);
+    };
 
-function GoalInput(orops) {
+    function addGoalHandler(){
+        props.onAddGoal(enteredGoalText);
+        setEnteredGoalText('');
+    }
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
-            <Button title='Add Goal' onPress={prop.onAddGoal} />
+            <TextInput 
+                style={styles.textInput} 
+                placeholder='Your course goal!' 
+                onChangeText={goalInputHandler}
+                value={enteredGoalText} />
+            <Button title='Add Goal' onPress={addGoalHandler} />
         </View>
     )
 }

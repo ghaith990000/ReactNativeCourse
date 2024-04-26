de6699e7e321ac1296059ae1de8a4b6bc024e0de
ghaith990000 <ghaith990000@gmail.com>
2024-04-26 14:30:31 +0300

    add cancel button to close the modal

--- App.js
+++ App.js
@@ -10,11 +10,16 @@ export default function App() {
       setModalIsVisible(true);
   }
 
+  function endAddGoalHandler(){
+    setModalIsVisible(false);
+  }
+
   function addGoalHandler(enteredGoalText){
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals, 
       {text: enteredGoalText, id: Math.random().toString()},
     ]);
+    endAddGoalHandler();
   };
 
   function deleteGoalHandler(id){
@@ -26,7 +31,7 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler} />
-      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList 
           data={courseGoals} 

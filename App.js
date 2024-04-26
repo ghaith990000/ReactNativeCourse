9220297795c96a0f89b032e8258d34f6587b2935
ghaith990000 <ghaith990000@gmail.com>
2024-04-26 14:56:16 +0300

    enhancing the style of the app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 export default function App() {
@@ -29,8 +30,10 @@ export default function App() {
   }
 
   return (
+    <>
+    <StatusBar style='light' />
     <View style={styles.appContainer}>
-      <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler} />
+      <Button title='Add New Goal' color="#a065ec" onPress={startAddGoalHandler} />
       <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList 
@@ -46,6 +49,7 @@ export default function App() {
 
       </View>
     </View>
+    </>
   );
 }
 
@@ -53,7 +57,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    backgroundColor: '#1e085a'
   },
   goalsContainer: {
     flex: 5

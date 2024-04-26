9363ae93f7dc4aeb6ecf226f3a3f149fa669b18f
ghaith990000 <ghaith990000@gmail.com>
2024-04-26 13:50:00 +0300

    extracted GoalInput component

--- App.js
+++ App.js
@@ -18,10 +18,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
-        <Button title='Add Goal' onPress={addGoalHandler} />
-      </View>
+      
       <View style={styles.goalsContainer}>
         <FlatList 
           data={courseGoals} 
@@ -44,22 +41,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
   },
   goalsContainer: {
     flex: 5
---
+++ components/GoalInput.js
@@ -0,0 +1,31 @@
+import { Button, StyleSheet, TextInput, View } from "react-native";
+
+function GoalInput(orops) {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
+            <Button title='Add Goal' onPress={prop.onAddGoal} />
+        </View>
+    )
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+})
